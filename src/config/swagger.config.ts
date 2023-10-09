423c3deb4bf0400c77c9b151a80eeee3e77637ef
Pablo González <pablogonzalezar@yahoo.com>
2023-10-09 13:45:36 -0300

    adding cors config updating swagger

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,23 @@
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export class SwaggerConfig {
+  private title: string = "Tracer.io";
+  private description: string = "Record and store data related to harvests, strains, and plants";
+  private version: string = "1.0.0";
+  private serverUrls: Array<string> = ["http://localhost:3000"];
+
+  public configureSwagger(app: INestApplication): void {
+    const swaggerOptions = new DocumentBuilder()
+      .setTitle(this.title)
+      .setDescription(this.description)
+      .setVersion(this.version)
+
+    this.serverUrls.forEach((serverUrl) => {
+        swaggerOptions.addServer(serverUrl);
+      });
+
+    const docs = SwaggerModule.createDocument(app, swaggerOptions.build());
+    SwaggerModule.setup('docs', app, docs);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,18 @@
 import { AppModule } from './app.module'
 import { NestFactory } from '@nestjs/core'
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerConfig } from './config/swagger.config'
 
 const PORT = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  const config = new DocumentBuilder()
-    .setTitle("Tracer.io")
-    .setDescription("Record and store data related to harvests, strains, and plants")
-    .setVersion("1.0.0")
-    .build()
+  const swaggerConfig = new SwaggerConfig();
+  swaggerConfig.configureSwagger(app);
 
-  const docs = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('docs', app, docs)
-
+  app.enableCors({
+    origin: true
+  })
   await app.listen(PORT)
   console.log(`Tracer.io running on port: ${PORT}`);
 }
